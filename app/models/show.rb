f550cf3f4d25f3c17efb4e49ee18473f85477b7d
Andrew Davisson <akdavisson4@gmail.com>
2019-06-25 16:27:42 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   
   def self.most_popular_show
     binding.pry
-    self.where('rating = ?', highest_rating)
+    most_popular = self.where('rating = ?', highest_rating)
   end
   
 end
